f5df584412a2debdba0eb2180958474d659b4e03
avinashtrivedi11 <116353157+avinashtrivedi11@users.noreply.github.com>
2023-07-19 15:42:13 +0530

    Update api_call.py

--- automation/scripts/api_call.py
+++ automation/scripts/api_call.py
@@ -5,7 +5,7 @@ UTILITY_APIS_BASE_URL = os.environ['UTILITY_APIS_BASE_URL']
 COMPANY_ID = os.environ['COMPANY_ID']
 
 db_setting_url = UTILITY_APIS_BASE_URL + "/api/utility_service/db_settings_configuration"
-db_setting_response = requests.get(url=db_setting_url, params={"company_id": COMPANY_ID})
+db_setting_response = requests.get(url=db_setting_url)
 db_setting_resp = db_setting_response.json()
 
 for key in db_setting_resp.keys():
